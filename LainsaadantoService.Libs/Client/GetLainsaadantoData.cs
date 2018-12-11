6b0cb76341e7c830f016b0ab2384fe21b6a9c7d5
astamaaria <myllymaki.asta@gmail.com>
2018-12-11 08:21:20 +0200

    Ulkoisen APin http-pyynnön käsittely muutettu GET:stä -> POST:ksi. Hakuehto lisätty. Data liikkuu nyt. Myös http-client vaihdettu http-client factoryyn.

--- LainsaadantoService.Libs/Client/GetLainsaadantoData.cs
+++ LainsaadantoService.Libs/Client/GetLainsaadantoData.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,30 @@ namespace LainsaadantoService.Libs.Client
 {
     public class GetLainsaadantoData :IGetLainsaadantoData
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public GetLainsaadantoData(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public async Task<DataModel> ReturnLainsaandantoData()
         {
-            using (var client = new HttpClient())
-            {
+            var client = _httpClientFactory.CreateClient();
+            string today = DateTime.Now.ToString("yyyy-dd-MM");
+            string search = $"{{ \"asettamisPaivaAlku\": \"2015-05-29\",\"asettamisPaivaLoppu\": \"{today}\",\"size\": 10, \"tyyppi\": [\"LAINSAADANTO\"],\"tila\":[\"KAYNNISSA\"]}}";
+
+            var httpContent = new StringContent(search);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
             var url = new Uri($"https://api.hankeikkuna.fi/api/v1/kohteet/haku");
 
-            var response = await client.GetAsync(url);
+            var response = await client.PostAsync(url, httpContent);
             string json;
             using (var content = response.Content)
             {
                 json = await content.ReadAsStringAsync();
             }
             return JsonConvert.DeserializeObject<DataModel>(json);
-            }
         }
 
     }
